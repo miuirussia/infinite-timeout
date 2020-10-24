67b19982f18ca65fe7ad4f126f0de410f0c0df92
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2020-10-24 13:39:47 +0300

    Add build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,21 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
 export default {
   input: 'src/main.js',
   output: {
-    file: 'dist/index.js',
+    file: 'dist/main.js',
     format: 'cjs'
-  }
+  },
+  plugins: [
+    babel({
+      presets: ['@babel/preset-flow'],
+      babelHelpers: 'bundled',
+    }),
+    nodeResolve({}),
+    commonjs(),
+    terser({ mangle: false }),
+  ],
 };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
 // @flow
+const MAX_TIMEOUT_MS = 0x7FFFFFFF;
+
+type PromiseRunner = () => Promise<mixed>
+type CancelFunction = () => void;
+
+export const scheduleTimeout = (f: PromiseRunner, timeout: number, loop?: boolean): CancelFunction => {
+  let timeoutId = null;
+
+  const startTimeout = (remainingTimeout: number) => {
+    if (remainingTimeout <= MAX_TIMEOUT_MS) {
+      timeoutId = setTimeout(async () => {
+        await f();
+        clearTimeout(timeoutId);
+        if (loop) {
+          timeoutId = startTimeout(timeout);
+        }
+      }, remainingTimeout);
+    } else {
+      timeoutId = setTimeout(() => {
+        clearTimeout(timeoutId);
+        timeoutId = startTimeout(remainingTimeout - MAX_TIMEOUT_MS);
+      }, MAX_TIMEOUT_MS);
+    }
+  };
+
+  startTimeout(timeout);
+
+  return () => {
+    clearTimeout(timeoutId);
+  };
+};
